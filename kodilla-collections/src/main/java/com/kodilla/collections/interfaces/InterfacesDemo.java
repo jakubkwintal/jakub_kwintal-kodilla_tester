cbfe03e689b1a72fe087ec7c173176a5aade1655
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-03 19:32:39 +0100

    Nowe

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/InterfacesDemo.java
@@ -0,0 +1,18 @@
+package com.kodilla.collections.interfaces;
+
+public class InterfacesDemo {
+    public static void main(String[] args) {
+        Square square = new Square(10.0);
+        showShapeDetails(square);
+
+        Circle circle = new Circle(7.0);
+        showShapeDetails(circle);
+
+        Triangle triangle = new Triangle(3.0, 4.0, 5.0);
+        showShapeDetails(triangle);
+    }
+    private static void showShapeDetails(Shape shape) {
+        System.out.println(shape.getArea());
+        System.out.println(shape.getPerimeter());
+    }
+}
---
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
@@ -0,0 +1,40 @@
+package com.kodilla.collections.interfaces;
+
+import java.util.Objects;
+
+public class Triangle implements Shape {
+    private double width;
+    private double height;
+    private double hypotenuse;
+
+    public Triangle(double width, double height, double hypotenuse) {
+        this.width = width;
+        this.height = height;
+        this.hypotenuse = hypotenuse;
+        }
+    public double getPerimeter() {
+        return width+height+hypotenuse;
+    }
+    public double getArea() {
+        return width*height/2;
+    }
+
+    public String toString() {
+        return "Triangle{" + "width=" + width + ", height=" + height + ", hypotenuse=" + hypotenuse + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Double.compare(triangle.width, width) == 0 &&
+                Double.compare(triangle.height, height) == 0 &&
+                Double.compare(triangle.hypotenuse, hypotenuse) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(width, height, hypotenuse);
+    }
+}
