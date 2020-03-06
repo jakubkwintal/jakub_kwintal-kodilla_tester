0cca7f7e23e61eddd545f902dfa02cca9c041821
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-06 20:00:17 +0100

    Niemutowalna klasa

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/ImmutableExample.java
@@ -0,0 +1,9 @@
+package com.kodilla.collections.adv.immutable;
+
+public class ImmutableExample {
+    public static void main(String[] args) {
+
+        Book book = new Book ("Robert Ludlum", "Tożsamość Bourne'a");
+book.
+    }
+}
