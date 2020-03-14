ada737f32d5b7bc380f28b28c1615f3a4fd7a076
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-14 16:26:40 +0100

    Student&Teacher - nieskończone...

---
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -0,0 +1,26 @@
+package com.kodilla.optional.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Application {
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Wojtek", "Pani Rzepka"));
+        students.add(new Student("Marysia", "Pan Domek"));
+        students.add(new Student("Franek", null));
+        students.add(new Student("Krysia", "Pani Kwiecień"));
+        students.add(new Student("Antek", null));
+
+        for (students:Student) {
+            Optional<Student> optionalStudent = Optional.ofNullable()
+
+        }
+              ) {
+            
+        }
+            
+        }
+    
+}
--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class TaskRepository {
     public static List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Wybór repertuaru na płytę", 2019 - 12 - 01, 2020 - 02 - 29));
-        tasks.add(new Task("Próby", 2020 - 03 - 01, 2020 - 10 - 14));
-        tasks.add(new Task("Projekt bookletu płyty", 2020 - 05 - 01, 2020 - 08 - 31));
-        tasks.add(new Task("Nagranie", 2020 - 10 - 15, 2020 - 10 - 31));
-        tasks.add(new Task("Montaż, mastering i produkcja płyty", 2020 - 11 - 01, 2020 - 12 - 15));
+//        tasks.add(new Task("Wybór repertuaru na płytę", 2019 - 12 - 01, 2020 - 02 - 29));
+//        tasks.add(new Task("Próby", 2020 - 03 - 01, 2020 - 10 - 14));
+//        tasks.add(new Task("Projekt bookletu płyty", 2020 - 05 - 01, 2020 - 08 - 31));
+//        tasks.add(new Task("Nagranie", 2020 - 10 - 15, 2020 - 10 - 31));
+//        tasks.add(new Task("Montaż, mastering i produkcja płyty", 2020 - 11 - 01, 2020 - 12 - 15));
         return tasks;
 
 
