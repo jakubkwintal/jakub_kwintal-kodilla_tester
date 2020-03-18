755f6c21e503dd8ffec2f344da71dbd3ca569d24
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-18 16:19:50 +0100

    Task - gotowe

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -1,18 +1,35 @@
 package com.kodilla.stream.homework;
 
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TaskManager {
     public static void main(String[] args) {
-        List<LocalDate> actualTasks = TaskRepository.getTasks()
+        TaskRepository.getTasks()
                 .stream()
-                .filter(u -> u.getDeadline().isBefore(LocalDate.now()))
-                .map(TaskManager::getActualTasks)
-                .collect(Collectors.toList());
-        System.out.println(actualTasks);
+                .filter(u -> u.getDeadline().isAfter(LocalDate.now()))
+                .map(u -> u.getName())
+                .forEach(un -> System.out.println(un));
     }
-
-    public static LocalDate getActualTasks(Task task) {return task.getDeadline();}
 }
+
+//package com.kodilla.stream.homework;
+//
+//        import java.time.LocalDate;
+//        import java.util.List;
+//        import java.util.stream.Collectors;
+//
+//public class TaskManager {
+//    public static void main(String[] args) {
+//       List<LocalDate> actualTasks = TaskRepository.getTasks()
+//                .stream()
+//                .filter(u -> u.getDeadline().isAfter(LocalDate.now()))
+//                .map(TaskManager::getActualTasks)
+//                .collect(Collectors.toList());
+//        {
+//
+//        }
+//        System.out.println(actualTasks);
+//    }
+//
+//    public static LocalDate getActualTasks(Task task) {return task.getDeadline();}
+//}
--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -1,18 +1,17 @@
 package com.kodilla.stream.homework;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TaskRepository {
     public static List<Task> getTasks() {
         List<Task> tasks = new ArrayList<>();
-//        tasks.add(new Task("Wybór repertuaru na płytę", 2019 - 12 - 01, 2020 - 02 - 29));
-//        tasks.add(new Task("Próby", 2020 - 03 - 01, 2020 - 10 - 14));
-//        tasks.add(new Task("Projekt bookletu płyty", 2020 - 05 - 01, 2020 - 08 - 31));
-//        tasks.add(new Task("Nagranie", 2020 - 10 - 15, 2020 - 10 - 31));
-//        tasks.add(new Task("Montaż, mastering i produkcja płyty", 2020 - 11 - 01, 2020 - 12 - 15));
+        tasks.add(new Task("Wybór repertuaru na płytę", LocalDate.of(2019, 10, 1), LocalDate.of(2019, 11, 30)));
+        tasks.add(new Task("Próby", LocalDate.of(2019, 12, 1), LocalDate.of(2020, 2, 28)));
+        tasks.add(new Task("Projekt bookletu płyty", LocalDate.of(2020, 2, 1), LocalDate.of(2020, 8, 31)));
+        tasks.add(new Task("Nagranie", LocalDate.of(2020, 10, 15), LocalDate.of(2020, 10, 31)));
+        tasks.add(new Task("Montaż, mastering i produkcja płyty", LocalDate.of(2020, 11, 1), LocalDate.of(2020, 12, 15)));
         return tasks;
-
-
     }
 }
