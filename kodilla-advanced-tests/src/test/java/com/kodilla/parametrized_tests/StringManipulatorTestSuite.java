add62eda4f2fd2d103a2c8ffa9adbd74b09e469d
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-22 20:59:37 +0100

    Person - jest error

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,26 +1,91 @@
-package com.kodilla.parametrized_tests;
+//package com.kodilla.parametrized_tests;
+//
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.Arguments;
+//import org.junit.jupiter.params.provider.CsvFileSource;
+//import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.MethodSource;
+//
+//import java.util.stream.Stream;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//public class StringManipulatorTestSuite {
+//
+//    private StringManipulator manipulator = new StringManipulator();
+//
+//
+//    @ParameterizedTest
+//    @MethodSource(value = "com.kodilla.parametrized_tests.StringSources#provideStringsForTestingLength")
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
+//
+//
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StringManipulatorTestSuite {
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
+//    public void shouldReverseStringWithLowerCase(String input, String expected) {
+//        assertEquals(expected, manipulator.reverseWithLowerCase(input));
+//    }
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/countNumberOfCommas.csv", numLinesToSkip = 1, delimiter = ':')
+//    public void shouldCountNumberOfCommas(String input, int expected) {
+//        assertEquals(expected, manipulator.countNumberOfCommas(input));
+//    }
 
-    private StringManipulator manipulator = new StringManipulator();
+//    private static Stream<Arguments> shouldCalculateStringLengthWithoutSpaces() {
+//        return Stream.of(
+//                Arguments.of("test", 4),
+//                Arguments.of("OtHEr ", 5),
+//                Arguments.of("E V e n t", 5),
+//                Arguments.of("null ", 4),
+//                Arguments.of("A", 1)
+//        );
+//    }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/stringWithLowerCase.csv", numLinesToSkip = 1)
-    public void shouldReverseStringWithLowerCase(String input, String expected) {
-        assertEquals(expected, manipulator.reverseWithLowerCase(input));
-    }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/countNumberOfCommas.csv", numLinesToSkip = 1, delimiter = ':')
-    public void shouldCountNumberOfCommas(String input, int expected) {
-        assertEquals(expected, manipulator.countNumberOfCommas(input));
-    }
+
+    //    @ParameterizedTest
+//    @MethodSource
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
+
+
+
+
+
+//    private static Stream<Arguments> provideStringsForTestingLength() {
+//        return Stream.of(
+//                Arguments.of("test", 4),
+//                Arguments.of("OtHEr ", 5),
+//                Arguments.of("E V e n t", 5),
+//                Arguments.of("null ", 4),
+//                Arguments.of("A", 1)
+//        );
+//    }
+//
+//    @ParameterizedTest
+//    @MethodSource(value = "provideStringsForTestingLength")
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
+
+
+
+
+//    @ParameterizedTest
+//    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
+//    public void shouldCalculateStringLengthWithoutSpaces(String input, int expected) {
+//        assertEquals(expected, manipulator.getStringLengthWithoutSpaces(input));
+//    }
+
+
+
 
 //    @ParameterizedTest
 //    @CsvSource(value = {"test,tset", "OtHEr,rehto", "EVent,tneve", "null,llun", "A,a"})
@@ -33,4 +98,4 @@ public class StringManipulatorTestSuite {
 //    public void shouldCountNumberOfCommas(String input, int expected) {
 //        assertEquals(expected, manipulator.countNumberOfCommas(input));
 //    }
-}
+//}
