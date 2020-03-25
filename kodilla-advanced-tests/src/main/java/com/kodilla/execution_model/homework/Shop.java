2aad9d8389508e30c19bd0d9f4d6f7d489c77402
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-25 18:00:07 +0100

    Shop, SchopTestSuite

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,53 +1,41 @@
-//package com.kodilla.execution_model.homework;
-//
-//import java.time.LocalDate;
-//import java.time.ZonedDateTime;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//public class Shop {
-//
-//    List<Order> orders = new ArrayList<>();
-////    private ZonedDateTime firstDate;
-////    private ZonedDateTime secondDate;
-//
-//    //    metoda wstawiająca zamówienie
-//    public void addOrder(Order order) {
-//        this.orders.add(order);
-//    }
-//
-//    // metoda zwracająca liczbę zamówień
-//    public int getSize() {
-//        return this.orders.size();
-//    }
-//
-//    //    metoda sumująca wartość zamówień
-//    public double sumOfOrdersPrice() {
-//        double sum = 0;
-//        for (Order order : orders) sum += order.getPrice();
-//        return sum;
-//
-//    }
-//
-//    public List<Order> orderListBetweenDates(LocalDate firstDate, LocalDate secondDate){
-//        return orders.stream().filter(order -> order.getDate().isAfter(firstDate) && order.getDate().isBefore(secondDate)).collect(Collectors.toList())
-//    }
-//
-//
-//    //    metoda filtrująca zamówienia pod wzglęsem zakresu dat
-////    public void getOrderFromRangeOfTime(LocalDate dateOfOrder) {
-////        if (firstDate.isBefore(ChronoZonedDateTime.from(dateOfOrder)) && secondDate.isAfter(ChronoZonedDateTime.from(dateOfOrder)));
-////        {
-////            return;
-////
-////    }
-//
-//
-////    metoda filtrująca zamówienia pod względem zakresu ceny
-////    public void getOrderFromRangeOfPrice(double priceOfOrder) {
-////            if (priceOfOrder >= )
-////        }
-//
-//
-//}
+package com.kodilla.execution_model.homework;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Shop {
+
+    List<Order> orders = new ArrayList<>();
+
+    //    metoda wstawiająca zamówienie
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
+
+    // metoda zwracająca liczbę zamówień
+    public int getSize() {
+        return this.orders.size();
+    }
+
+    //    metoda sumująca wartość zamówień
+    public double sumOfOrdersPrice() {
+        double sum = 0;
+        for (Order order : orders) sum += order.getPrice();
+        return sum;
+    }
+
+//    metoda zwracająca zamówienia z zakresu dat
+
+    public List<Order> orderListBetweenDates(LocalDate firstDate, LocalDate secondDate) {
+        return orders.stream().filter(order -> order.getDate().isAfter(firstDate.minusDays(1))
+                && order.getDate().isBefore(secondDate.plusDays(1))).collect(Collectors.toList());
+    }
+
+    //    metoda filtrująca zamówienia pod względem zakresu ceny
+    public void orderListFromRangeOfPrices(double lowestPrice, double highestPrice) {
+        double i = 0.00;
+        while (lowestPrice <= highestPrice) {i++;}
+    }
+}
