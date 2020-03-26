f3bc67d9efb58732c1334e15ba894e71c19fef19
Jakub Kwintal <jakubkwintal@yahoo.com>
2020-03-26 07:11:33 +0100

    WarehouseApp - działa

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -1,12 +1,20 @@
 package com.kodilla.exception.homework;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class WarehouseApp {
     public static void main(String[] args) {
+        Warehouse warehouse = new Warehouse();
+        warehouse.addOrder(new Order("2020/03/132"));
+        warehouse.addOrder(new Order("2020/03/133"));
+        warehouse.addOrder(new Order("2020/03/134"));
+        warehouse.addOrder(new Order("2020/02/135"));
 
+        try {
+            warehouse.getOrder("2020/03/133");
+        } catch (OrderDoesntExistException e) {
+            System.out.println("Unfortunately, the order with the given number doesn't exist.");
+        } finally {
+            System.out.println("Have a good day!");
+        }
 
     }
 }
